02cb71140ee347a73651282f772109ba85f550cf
Shubham9492 <sutharshubham84931@gmail.com>
2024-06-18 11:13:49 +0530

    submit handler

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,27 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(user);
+    setUser({
+      username: "",
+      password: "",
+    })
+  }
+
   return (
     <div className='min-w-96 mx-auto'>
       <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100'>
         <h1 className='text-3xl font-bold text-center text-gray-300'>Login</h1>
-        <form action="">
+        <form onSubmit={onSubmitHandler} action="">
           <div>
             <label className='label p-2'>
               <span className='text-base label-text'>Username</span>
             </label>
-            <input className='w-full input input-bordered h-10' type="text" placeholder='Username' />
+            <input value={user.username} onChange={(e)=>setUser({...user,username:e.target.value})} className='w-full input input-bordered h-10' type="text" placeholder='Username' />
           </div>
           <div>
             <label className='label p-2'>
               <span className='text-base label-text'>Password</span>
             </label>
-            <input className='w-full input input-bordered h-10' type="password" placeholder='Password' />
+            <input value={user.password} onChange={(e)=>setUser({...user,password:e.target.value})} className='w-full input input-bordered h-10' type="password" placeholder='Password' />
           </div>
-          <p className='text-center my-2'>Don't have an account? <Link to="/signup"> SignUp </Link></p>
+          <p className='text-center my-2'>Don't have an account? <Link to="/register"> SignUp </Link></p>
           <div>
-            <button className='btn btn-block btn-sm mt-2 border border-slate-700'>Signup</button>
+            <button type='submit' className='btn btn-block btn-sm mt-2 border border-slate-700'>Login</button>
           </div>
         </form >
       </div >
@@ -29,4 +43,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Login;
